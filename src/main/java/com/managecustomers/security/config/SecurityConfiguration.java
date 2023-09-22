4bed6d28187767701f1b9a287eb282547963d86f
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2023-09-22 18:23:04 +0100

    add new cros orrigin addresses for localhost

--- src/main/java/com/managecustomers/security/config/SecurityConfiguration.java
+++ src/main/java/com/managecustomers/security/config/SecurityConfiguration.java
@@ -47,7 +47,9 @@ public class SecurityConfiguration {
 
           String allowsOrigin = "http://127.0.0.1:5501";
           String allowsOrigin2 = "https://manage-customers-4x6n9.ondigitalocean.app";
-          configuration.setAllowedOrigins(List.of(allowsOrigin,allowsOrigin2));
+          String allowsOrigin3 = "https://localhost:3000";
+          String allowsOrigin4 = "http://localhost:3000";
+          configuration.setAllowedOrigins(List.of(allowsOrigin,allowsOrigin2,allowsOrigin3,allowsOrigin4));
           configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT", "HEAD", "OPTIONS","DELETE"));
           configuration.setAllowedHeaders(Arrays.asList("Authorization","Content-Type","accept", "accept-language", "content-type",  "authorization", "moduleid", "tabid", "x-dnn-moniker"));
 
